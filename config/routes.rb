129edfaeccffdb89b5790cbd70c0ef61b8e02a94
atsumichatnoir <a.i.0789.a@gmail.com>
2017-07-24 02:13:49 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
   get 'cafe/homepage'
 
   get 'nemunemu/homepage'
   get 'nemunemu/nemunemu'
-
+  root 'nemunemu#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
